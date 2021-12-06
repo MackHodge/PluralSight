bcb3ec7f9a445c82c96e4154d0ef1291831b28cb
Mack Hodge <mack.hodge@helplinenl.onmicrosoft.com>
2021-12-07 00:37:14 +0100

    init commit

---
+++ Lambda1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lambda1
+{
+    internal class Program
+    {
+        delegate int MultiPlyDelegate(int a , int b);
+        delegate int MultilyDelegate2(int a , int b);
+        delegate int EmptyParms();
+        public static void Main(string[] args)
+        {
+            MultiPlyDelegate ad = (Par1, Par2) => Par1 * Par2;
+            int result = ad(8, 3);
+
+            MultilyDelegate2 ml = (Par1, Par2) => Par1 * Par2;
+            int totalResult = ml(result , 3);
+
+            Console.WriteLine(totalResult);
+            
+            //what if there is in par 
+            //create a methode of both delegetes and call them here 
+            
+            
+        }
+    }
+}
