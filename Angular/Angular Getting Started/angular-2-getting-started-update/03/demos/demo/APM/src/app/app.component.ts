43bc15c1dbdee34494d71813fce795d3b8afe720
Mack Hodge <mack.hodge@helplinenl.onmicrosoft.com>
2022-02-04 11:09:36 +0100

    Angular Getting Started

--- Angular/Angular Getting Started/angular-2-getting-started-update/03/demos/demo/APM/src/app/app.component.ts
+++ Angular/Angular Getting Started/angular-2-getting-started-update/03/demos/demo/APM/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  //selector is the name of the component when we use it as directive  
+ selector: 'pm-root' , 
+ template: `
+ <div> 
+<h1>{{pagetitle}} </h1>
+<div>My First Component</div>
+<p> {{number}} </p>
+<pm-products></pm-products>
+ </div>`  
+})    
+ 
 export class AppComponent {
-  title = 'Angular: Getting Started';
+pagetitle : string = 'Product Management'  
+number : string = ""
 }
